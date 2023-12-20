ef3e462f9c01f18bf26435d1fcfa00475913f804
s0a1e2 <oops2198@gmail.com>
2023-12-20 10:13:20 +0330

    config database to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DevsController } from './devs.controller';
 import { DevsService } from './devs.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Dev, DevSchema } from './schemas/dev.schema';
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/devs-devProblem'),
+    MongooseModule.forFeature([{ name: Dev.name, schema: DevSchema }]),
+  ],
   controllers: [DevsController],
   providers: [DevsService],
 })
