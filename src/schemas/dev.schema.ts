039c0be74a96a12cbea68795a28594cb5016053c
s0a1e2 <oops2198@gmail.com>
2023-12-20 10:13:07 +0330

    config schema

--- src/schemas/dev.schema.ts
+++ src/schemas/dev.schema.ts
@@ -1,1 +1,30 @@
-export class Dev {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type IdeaDocument = HydratedDocument<Dev>;
+
+@Schema()
+export class Dev {
+  @Prop()
+  title: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  creator: string;
+
+  @Prop()
+  idea: string;
+
+  @Prop()
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+
+  @Prop()
+  deletedAt: Date;
+}
+
+export const DevSchema = SchemaFactory.createForClass(Dev);
