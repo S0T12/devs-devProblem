743340d76280f2b7a835f8033cc11e298a33b00a
s0a1e2 <oops2198@gmail.com>
2024-01-24 21:38:12 +0330

    update devs service

--- src/devs.service.ts
+++ src/devs.service.ts
@@ -1,18 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
 import { CreateDevDto } from './dto/create-dev.dto';
 import { UpdateDevDto } from './dto/update-dev.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Dev } from './schemas/dev.schema';
 import { Model } from 'mongoose';
 import { ObjectId } from 'mongodb';
+import { ClientProxy } from '@nestjs/microservices';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
 export class DevsService {
-  constructor(@InjectModel(Dev.name) private devModel: Model<Dev>) {}
+  constructor(
+    @InjectModel(Dev.name) private devModel: Model<Dev>,
+    @Inject('USERS_SERVICE') private readonly usersClient: ClientProxy,
+    @Inject('IDEAS_SERVICE') private readonly ideasClient: ClientProxy,
+  ) {}
   async create(createDevDto: CreateDevDto) {
     try {
+      const user = await firstValueFrom(
+        this.usersClient.send<string>('findByUsername', createDevDto.creator),
+      );
+      if (!user) return new BadRequestException('User Not Found');
+
+      const idea = await firstValueFrom(
+        this.ideasClient.send<ObjectId>('findOneIdea', createDevDto.ideaID),
+      );
+      if (!idea) return new BadRequestException('User Not Found');
+
       const dev = new this.devModel(createDevDto);
-      await dev.save();
+      return await dev.save();
     } catch (error) {
       throw error;
     }
@@ -27,10 +43,11 @@ export class DevsService {
   }
 
   async update(id: ObjectId, updateDevDto: UpdateDevDto): Promise<any> {
-    return await this.devModel.updateOne(id, updateDevDto);
+    return await this.devModel.updateOne({ _id: id }, updateDevDto);
   }
 
   async remove(id: ObjectId): Promise<any> {
-    return await this.devModel.deleteOne(id);
+    const objectId: ObjectId = new ObjectId(id);
+    return await this.devModel.deleteOne(objectId);
   }
 }
