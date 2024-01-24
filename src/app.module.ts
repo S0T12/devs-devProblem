2af4056389d69a650dd1878523b9056b437d9cb7
s0a1e2 <oops2198@gmail.com>
2024-01-24 21:26:23 +0330

    import users and ideas service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,33 @@ import { DevsController } from './devs.controller';
 import { DevsService } from './devs.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Dev, DevSchema } from './schemas/dev.schema';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 @Module({
   imports: [
+    ClientsModule.register([
+      {
+        name: 'USERS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'users_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+      {
+        name: 'IDEAS_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost:5672'],
+          queue: 'ideas_queue',
+          queueOptions: {
+            durable: false,
+          },
+        },
+      },
+    ]),
     MongooseModule.forRoot('mongodb://localhost:27017/devs-devProblem'),
     MongooseModule.forFeature([{ name: Dev.name, schema: DevSchema }]),
   ],
